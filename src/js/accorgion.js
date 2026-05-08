ab4420bb86e51e21369e2e765ce71dae59123117
Nikita <remnovmikita@gmail.com>
2026-05-09 00:23:22 +0300

    new libraly acordeon add section faq

--- src/js/accorgion.js
+++ src/js/accorgion.js
@@ -1,14 +1,36 @@
-import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css";
-console.log("Аккардеон работает");
+import accordion from "accordion";
+import "accordion/src/accordion.css";
 
-const el = document.querySelector(".accordion");
-console.log(el);
-
+var el = document.querySelector(".accordion");
 new Accordion(el, {
-  duration: 400,
-  showMultiple: true,
   onToggle: (fold, isOpen) => {
-    alert("onToggle вызван!"); // должно всплыть при клике
+    const item = fold.el; 
+    const icon = fold.el.querySelector("svg use");
+
+     if (isOpen) {
+      icon.setAttribute("href", "/img/sprite.svg#icon-icon-up");
+    } else {
+      icon.setAttribute("href", "/img/sprite.svg#icon-icon-down");
+    }
+
+    if (!isOpen) {
+      item.style.backgroundColor = "";
+      return;
+    }
+    if (item.classList.contains("first")) {
+      item.style.backgroundColor = "#f7d62e"; 
+    }
+    if (item.classList.contains("second")) {
+      item.style.backgroundColor = "#fd8ff0"; 
+    }
+    if (item.classList.contains("third")) {
+      item.style.backgroundColor = "#74d2ee"; 
+    }
+    if (item.classList.contains("fourth")) {
+      item.style.backgroundColor = "#a4c73a"; 
+    }
+    if (item.classList.contains("fifth")) {
+      item.style.backgroundColor = "#fdab4f"; 
+    }
   }
 });
