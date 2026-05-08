fe7263eddcb91b424905391852d2041a7fd106a9
Nikita <remnovmikita@gmail.com>
2026-05-08 22:40:02 +0300

    add privacy section faq & change footer

--- src/js/accorgion.js
+++ src/js/accorgion.js
@@ -1,12 +1,14 @@
 import Accordion from "accordion-js";
-import "accordion-js/dist/accordion.min.css"
+import "accordion-js/dist/accordion.min.css";
+console.log("Аккардеон работает");
+
 const el = document.querySelector(".accordion");
+console.log(el);
 
 new Accordion(el, {
   duration: 400,
   showMultiple: true,
   onToggle: (fold, isOpen) => {
-    console.log("Fold:", fold);
-    console.log("Is open:", isOpen);
+    alert("onToggle вызван!"); // должно всплыть при клике
   }
 });
