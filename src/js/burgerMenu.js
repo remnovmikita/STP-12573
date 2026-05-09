490d7a81b2f55a2d75415736776a2574249d209f
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-05-09 04:36:45 +0300

    add mobile menu

--- src/js/burgerMenu.js
+++ src/js/burgerMenu.js
@@ -1,1 +1,12 @@
-console.log("Java працює");
+const openMenuBtn = document.querySelector('.menu-btn');
+const closeMenuBtn = document.querySelector('.btn-closed-mobile-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+openMenuBtn.addEventListener('click', () => {
+  mobileMenu.classList.add('is-open');
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  console.log('close menu');
+  mobileMenu.classList.remove('is-open');
+});
