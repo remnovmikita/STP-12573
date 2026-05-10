51838ee0990fde4e86867ef3ab21994fd6b04f46
Nikita <remnovmikita@gmail.com>
2026-05-10 03:50:43 +0300

    fix faq done & rewiews

--- src/js/accorgion.js
+++ src/js/accorgion.js
@@ -1,36 +1,16 @@
-import accordion from "accordion";
-import "accordion/src/accordion.css";
+const accordionItems = document.querySelectorAll(".main-acc");
 
-var el = document.querySelector(".accordion");
-new Accordion(el, {
-  onToggle: (fold, isOpen) => {
-    const item = fold.el; 
-    const icon = fold.el.querySelector("svg use");
+accordionItems.forEach(item => {
+  const header = item.querySelector(".upper-case");
 
-     if (isOpen) {
-      icon.setAttribute("href", "/img/sprite.svg#icon-icon-up");
-    } else {
-      icon.setAttribute("href", "/img/sprite.svg#icon-icon-down");
-    }
+  header.addEventListener("click", () => {
 
-    if (!isOpen) {
-      item.style.backgroundColor = "";
-      return;
-    }
-    if (item.classList.contains("first")) {
-      item.style.backgroundColor = "#f7d62e"; 
-    }
-    if (item.classList.contains("second")) {
-      item.style.backgroundColor = "#fd8ff0"; 
-    }
-    if (item.classList.contains("third")) {
-      item.style.backgroundColor = "#74d2ee"; 
-    }
-    if (item.classList.contains("fourth")) {
-      item.style.backgroundColor = "#a4c73a"; 
-    }
-    if (item.classList.contains("fifth")) {
-      item.style.backgroundColor = "#fdab4f"; 
-    }
-  }
+  // accordionItems.forEach(acc => {
+  //     if (acc !== item) {
+  //       acc.classList.remove("open");
+  //     }
+  //   });
+
+    item.classList.toggle("open");
+  });
 });
